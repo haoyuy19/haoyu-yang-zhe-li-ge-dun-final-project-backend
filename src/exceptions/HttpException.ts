3fb20f120a2d621953271009ec181d74bcde5c93
DDG <dunge0318@gmail.com>
2020-12-10 10:55:48 -0800

    auth

--- src/exceptions/HttpException.ts
+++ src/exceptions/HttpException.ts
@@ -1,10 +1,13 @@
 class HttpException extends Error {
-  public status: number;
-  public message: string;
-  constructor(status: number, message: string) {
+  status: number;
+  message: string;
+  errors?: any;
+
+  constructor(status: number, message: string, errors?: any) {
     super(message);
     this.status = status;
     this.message = message;
+    this.errors = errors;
   }
 }
 
